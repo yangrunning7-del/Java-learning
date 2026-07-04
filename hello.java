33d86717cde8b1f32c924d4b02ec9220a7545fa4
yangrunning7-del <yangrunning7@gmail.com>
2026-07-04 23:39:59 +0800

    Add Chinese greetings to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
 public class hello {
     public static void main(String[] args) {
+        System.out.println("你好");
+        System.out.println("加油");
         System.out.println("hello world");
     }
 }
