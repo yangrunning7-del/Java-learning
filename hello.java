c08920b10824a568d6431d4d276154506d18d604
yangrunning7 <yangrunning7@gmail.com>
2026-07-06 21:35:39 +0800

    first java program

--- hello.java
+++ hello.java
@@ -1,7 +1,5 @@
-public class hello {
-    public static void main(String[] args) {
-        System.out.println("你好");
-        System.out.println("加油");
-        System.out.println("hello world");
+public class hello{
+    public static void main(String[] args){
+        System.out.println("hello fuck man");
     }
 }
