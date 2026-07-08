1b7d7169b5c5dc8235c103c84483c03ab7956a8a
yangrunning7 <yangrunning7@gmail.com>
2026-07-09 00:55:32 +0800

    9 July

---
+++ object_oriented_programming/src/demo03.java
@@ -0,0 +1,27 @@
+public class demo03 {
+    public static void main(String[] args) {
+        /*
+        关于面向对象的阶段性总结
+
+        1、类与对象：
+            1、类是一个抽象的模板，对象是一个具体的实例
+                -- 静态的属性：属性
+                -- 动态的行为：方法
+                    -- 方法；定义，调用
+            2、对像的创建和使用，引用
+                -- 对象必须通过 new 关键字来实现，创建对象的时候，需要构造器，构造器的命名与类一样
+                -- Person student = new Person()
+                -- 对象的属性，student.score
+                -- 对象的方法，student.playGame()
+                -- 对象的引用，通过引用来操作：栈 --> 堆
+        2、属性，字段Filed 成员变量
+            默认初始化
+                -- 数字：0，0.0
+                -- char：u000
+                -- boolean: false
+                -- 引用：null
+            修饰符 属性类型 属性名 = 属性值
+            public string name = "张三";
+         */
+    }
+}
