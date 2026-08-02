06571a27eef363839bfd34a8c265bec50e3a5dfd
yangrunning7 <yangrunning7@gmail.com>
2026-08-02 10:59:28 +0800

    2 August

--- exception/practice/Test.java
+++ exception/practice/Test.java
@@ -6,18 +6,23 @@ public class Test {
     public static void main(String[] args) {
         //创建GirlFriend对象
         GirlFriend gf = new GirlFriend();
-        //创建Scanner对象用于从键盘接收
-        try {
-            Scanner r = new Scanner(System.in);
-            System.out.println("Enter your name: ");
-            gf.setName(r.nextLine());
+        while (true) {
+            //创建Scanner对象用于从键盘接收
+            try {
+                Scanner r = new Scanner(System.in);
+                System.out.println("Enter your name: ");
+                gf.setName(r.nextLine());
 
-            System.out.println("Enter your age: ");
-            gf.setAge(r.nextInt());
-        } catch (MyException e) {
-            e.printStackTrace();
-        } catch (AgeOutOfBoundsException e) {
-            e.printStackTrace();
+                System.out.println("Enter your age: ");
+                gf.setAge(r.nextInt());
+                break;
+            } catch (MyException e) {
+                e.printStackTrace();
+                continue;
+            } catch (AgeOutOfBoundsException e) {
+                e.printStackTrace();
+                continue;
+            }
         }
 
         System.out.println("your girlFriend: " + gf.getName() + " : " + gf.getAge());
